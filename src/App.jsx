e5e03ea08feee419f60090bda4d93c658caa272c
Dennys Nascimento <dhnascimento91@gmail.com>
2022-11-14 19:22:08 -0300

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import HomePage from './pages/HomePage';
-import AboutPages from './pages/AboutPage';
-import NavBar from './component/NavBar';
-import Usuario from './pages/Usuario';
-import { PrivateRoute } from './privateRoute';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import AboutPages from "./pages/AboutPage";
+import NavBar from "./component/NavBar";
+import Usuario from "./pages/Usuario";
+import { PrivateRoute } from "./privateRoute";
 
 function App() {
   return (
-      <Router>
-          <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/usuario" element={<PrivateRoute><Usuario /></PrivateRoute>} />
-          <Route path="/about" element={<AboutPages />} />
-        </Routes>
-      </Router>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/usuario"
+          element={
+            <PrivateRoute>
+              <Usuario />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/about" element={<AboutPages />} />
+      </Routes>
+    </Router>
   );
 }
 
