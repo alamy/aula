7c35f3cac87c494ccf3e05b299b020fbf7aaef04
Wanderson R Marques <wrm@cin.ufpe.br>
2022-11-16 10:52:07 -0300

    fix: change the menu to the correct menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import HomePage from './pages/HomePage';
 import AboutPages from './pages/AboutPage';
-import ResponsiveAppBar from './component/NavBar2';
+import NavBar from './component/NavBar';
 import Usuario from './pages/Usuario';
 import { PrivateRoute } from './privateRoute';
 import CountPage from './pages/CountPage'
@@ -10,7 +10,7 @@ import CountPage from './pages/CountPage'
 function App() {
   return (
     <Router>
-          <ResponsiveAppBar />
+          <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/usuario" element={<PrivateRoute><Usuario /></PrivateRoute>} />
---
+++ src/theme/index.js
@@ -0,0 +1,22 @@
+import { createTheme } from '@mui/material'
+
+export const Theme = createTheme({
+    palette: {
+        primary:{
+            main: '#67159C',
+            dark: '#49007a',
+            light: '#946baf',
+            contrastText: '#fff',
+        },
+        secondary: {
+            main: '#E82D92',
+            dark: '#d10873',
+            light: '#ea7cb7',
+            contrastText: '#fff',
+        },
+        background: {
+            default: '#19181F',
+            paper: '#1E1E26'
+        }
+    }
+})
