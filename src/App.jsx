296c4aba22226afebfac0d9ccd302c2131bff1af
Wanderson R Marques <wrm@cin.ufpe.br>
2022-11-16 08:25:44 -0300

    fix: added vite in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import './App.css'
 import HomePage from './pages/HomePage';
 import AboutPages from './pages/AboutPage';
 import NavBar from './component/NavBar';
@@ -9,7 +9,7 @@ import CountPage from './pages/CountPage'
 
 function App() {
   return (
-      <Router>
+    <Router>
           <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -18,7 +18,7 @@ function App() {
           <Route path="/contador" element={<CountPage />} />
         </Routes>
       </Router>
-  );
+  )
 }
 
-export default App;
+export default App
